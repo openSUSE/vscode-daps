9aba17d090e0a92e7054a045381cb468ceeef09f
Tomáš Bažant <tbazant@suse.cz>
2023-06-27 18:33:33 +0200

    Build is now contextual from explorer

--- extension.js
+++ extension.js
@@ -19,7 +19,7 @@ function activate(context) {
 
 	console.log('Congratulations, your extension "daps" is now active!');
 	let disposable1 = vscode.commands.registerCommand('daps.validate', (DCfile) => validate(DCfile));
-	let disposable2 = vscode.commands.registerCommand('daps.build', () => buildTarget());
+	let disposable2 = vscode.commands.registerCommand('daps.build', (DCfile) => buildTarget(DCfile));
 	let disposable3 = vscode.commands.registerCommand('daps.XMLformat', () => XMLformat());
 	context.subscriptions.push(disposable1, disposable2, disposable3);
 }
@@ -33,25 +33,16 @@ function XMLformat() {
 		console.error(err);
 	}
 }
-
+/**
+ * @description builds HTML or PDF targets given DC file
+ * @param {obj} DCfile URI from context command (optional)
+ * @returns true or false depending on how the build happened
+ */
 async function buildTarget() {
-	var DCfile, buildTarget;
-	const dapsConfig = vscode.workspace.getConfiguration('daps');
-	// change working directory to current workspace
-	try {
-		process.chdir(workspaceFolderUri.path);
-		console.log('cwd is ' + workspaceFolderUri.path);
-	} catch (err) {
-		console.error('cwd: ' + err);
-	}
-	// try if DC file is included in settings or get it from user
-	if (DCfile = dapsConfig.get('DCfile')) {
-		console.log('DC file from config: ' + DCfile);
-	} else {
-		DCfile = await vscode.window.showQuickPick(getDCfiles());
-		console.log('DC file form picker: ' + DCfile);
-	}
+	var buildTarget;
+	var DCfile = await getDCfile(arguments[0]);
 	// try if buildTarget is included in settings or get it from user
+	const dapsConfig = vscode.workspace.getConfiguration('daps');
 	if (buildTarget = dapsConfig.get('buildTarget')) {
 		console.log('buildTarget from config: ' + buildTarget);
 	} else {
@@ -63,6 +54,9 @@ async function buildTarget() {
 	if (DCfile && buildTarget) {
 		try {
 			vscode.window.showInformationMessage('Running ' + dapsCmd);
+			// change working directory to current workspace
+			process.chdir(workspaceFolderUri.path);
+			console.log('cwd is ' + workspaceFolderUri.path);
 			let cmdOutput = execSync(dapsCmd);
 			let targetBuild = cmdOutput.toString().trim();
 			if (buildTarget == 'html') {
@@ -77,52 +71,70 @@ async function buildTarget() {
 					vscode.env.clipboard.writeText(targetBuild);
 				}
 			});
+			return true;
 		} catch (err) {
 			vscode.window.showErrorMessage('Build failed: ' + err);
 		}
 	}
+	return false;
 }
-
+/**
+ * @description validates documentation identified by DC file
+ * @param {obj} DCfile URI from context command (optional)
+ * @returns true or false depending on how validation happened
+ */
 async function validate() {
-	// change working directory to current workspace
-	try {
-		process.chdir(workspaceFolderUri.path);
-		console.log('cwd is ' + workspaceFolderUri.path);
-	} catch (err) {
-		console.error('cwd: ' + err);
-	}
-	var DCfile;
-	if (DCfile = arguments[0]) {
-		DCfile = DCfile.path.split('/').pop();
-		console.log('DCfile from context: ' + DCfile + ', ' + typeof DCfile);
-	} else {
-		const dapsConfig = vscode.workspace.getConfiguration('daps');
-		// try if DC file is included in settings or get it from user
-		if (DCfile = dapsConfig.get('DCfile')) {
-			console.log('DC file from config: ' + DCfile);
-		} else {
-			DCfile = await vscode.window.showQuickPick(getDCfiles());
-			console.log('DC file form picker: ' + DCfile);
-		}
-	}
-	// assemble daps command
+	var DCfile = await getDCfile(arguments[0]);
 	if (DCfile) {
+		// assemble daps command
 		const dapsCmd = 'daps -d ' + DCfile + ' validate';
 		try {
 			vscode.window.showInformationMessage('Running ' + dapsCmd);
+			// change working directory to current workspace
+			process.chdir(workspaceFolderUri.path);
+			console.log('cwd is ' + workspaceFolderUri.path);
 			execSync(dapsCmd);
 			vscode.window.showInformationMessage('Validation succeeded.');
+			return true;
 		} catch (err) {
 			vscode.window.showErrorMessage('Validation failed: ' + err);
 		}
 	}
+	return false;
 }
 
-function getDCfiles() {
-	// get list of DC files in the workspace
-	console.log(workspaceFolderUri.path);
-	const items = fs.readdirSync(workspaceFolderUri.path).filter(it => it.startsWith('DC-'));
-	return items;
+/**
+ * @description compiles list of DC files in gicen directory
+ * @param {obj} URI of a folder to get DC files from
+ * @returns {array} of DC files from the current directory
+ */
+function getDCfiles(folderUri) {
+	console.log('folder: ' + folderUri.path);
+	var allFiles = fs.readdirSync(folderUri.path);
+	console.log('no of all files: ' + allFiles.length)
+	var DCfiles = allFiles.filter(it => it.startsWith('DC-'));
+	console.log('no of DC files: ' + DCfiles.length);
+	return DCfiles;
+}
+/**
+ * @description picks DC file from various sources
+ * @param {obj} DCfile URI from context command (optional)
+ * @returns {string} DCfile name
+ */
+async function getDCfile() {
+	var DCfile;
+	const dapsConfig = vscode.workspace.getConfiguration('daps');
+	if (DCfile = arguments[0]) { // check if DCfile URI was passed as argument
+		// use only the base name from the full URI
+		DCfile = DCfile.path.split('/').pop();
+		console.log('DCfile from context: ' + DCfile);
+	} else if (DCfile = dapsConfig.get('DCfile')) { // try if DC file is included in settings.json
+		console.log('DC file from config: ' + DCfile);
+	} else { // get DC file from picker
+		DCfile = await vscode.window.showQuickPick(getDCfiles(workspaceFolderUri));
+		console.log('DC file form picker: ' + DCfile);
+	}
+	return DCfile;
 }
 // This method is called when your extension is deactivated
 function deactivate() { }
